174d6a2c1ba62063f3c688e614d14a44142d7f39
Roger <12768403+rogix@users.noreply.github.com>
2019-10-17 17:55:09 -0300

    chores: fetch users data main page with axios

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,20 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import api from '../../services/api';
 
 import { Container } from './styles';
 
 export default function Main() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await api.get('/users');
+
+      setData(result.data);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <Container>
       <section>
         <h1>Author</h1>
         <ul>
-          <li>James Clarck</li>
-          <li>James Clarck</li>
-          <li>James Clarck</li>
-          <li>James Clarck</li>
-          <li>James Clarck</li>
-          <li>James Clarck</li>
-          <li>James Clarck</li>
+          {data.map(user => (
+            <li key={user.id}>
+              <span>{user.name}</span>
+            </li>
+          ))}
         </ul>
       </section>
     </Container>
