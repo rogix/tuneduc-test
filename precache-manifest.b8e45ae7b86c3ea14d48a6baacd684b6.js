aa9c448d9d25a82242cdb9bcb6355c0338307325
Roger <12768403+rogix@users.noreply.github.com>
2019-10-18 01:28:23 -0300

    Updates

---
+++ precache-manifest.b8e45ae7b86c3ea14d48a6baacd684b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5808a793bf85e25479833248fde38ac",
+    "url": "/tuneduc-test/index.html"
+  },
+  {
+    "revision": "76d0c9293a50f218dfc8",
+    "url": "/tuneduc-test/static/js/2.849a5792.chunk.js"
+  },
+  {
+    "revision": "dd88f96268bc10bce8de",
+    "url": "/tuneduc-test/static/js/main.e1368d18.chunk.js"
+  },
+  {
+    "revision": "e7f9df95050ebf00e986",
+    "url": "/tuneduc-test/static/js/runtime-main.deb39094.js"
+  },
+  {
+    "revision": "4f256c4836274ca69df5aa54f240a2fd",
+    "url": "/tuneduc-test/static/media/logo.4f256c48.svg"
+  }
+]);
